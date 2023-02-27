13f8796401e1db05cef8b6610db8f93f6a1bf304
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-02-27 00:49:51 -0500

    ---minor CSS

--- unfinitebeta/src/Pages/ResultsPage.js
+++ unfinitebeta/src/Pages/ResultsPage.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import Roadmap from '../Components/Roadmap';
 // import '../css/ResultsPage.css';
+import isAuthenticated from '../Functions/isAuthenticated';
+import { useNavigate } from "react-router-dom";
 
 /* function that takes in a list of items and lists them on the left as a clickable list */
 
 function ResultsPage({items}) {
+
+    const [userstatus , setUserstatus] = useState(null);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        const checkAuth = async () => {
+          const authenticated = await isAuthenticated();
+          setUserstatus(authenticated);
+          if (!authenticated) {
+            navigate('/login');
+          }
+        };
+    
+        checkAuth();
+    
+      }, []);  
+
     return (
         <div>
             <Roadmap />
