50a5732d6feefe9a4bc9b2801cba6059d2e0cfa7
Lami <olamide2olowoniyi@gmail.com>
2023-04-08 15:15:18 -0400

    Added mobilecheck directs mobile users to desktop

--- unfinitebeta/src/Pages/LandingPage.js
+++ unfinitebeta/src/Pages/LandingPage.js
@@ -3,6 +3,7 @@ import '../css/LandingPage.css';
 import { Link } from "react-router-dom";
 import isAuthenticated from '../Functions/isAuthenticated';
 import { useNavigate } from "react-router-dom";
+import MobileCheck from "../Functions/MobileCheck";
 import ReactGA from 'react-ga';
 ReactGA.initialize('G-8YXPLS55QD');
 
@@ -33,6 +34,7 @@ function LandingPage() {
         <div className='bodylandingpage'>
             <div className="LandingPage">
                 <h1 className="logocenter">UNFINITE <span className="beta-symbol">&beta;</span></h1>
+                <MobileCheck />
                 <p>The new way to learn something.</p>
                 <Link className='signupbutton' to='/signup'>Sign Up</Link>
                 <Link className='loginbutton' to='/login'>Log in</Link>
