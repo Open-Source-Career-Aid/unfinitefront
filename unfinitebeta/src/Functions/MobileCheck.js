284d80a82b1f97b3808c275dd40ecaa5c21dab74
Lami <olamide2olowoniyi@gmail.com>
2023-04-08 21:49:24 -0400

    converted img bg to webp and reduced size

--- unfinitebeta/src/Functions/MobileCheck.js
+++ unfinitebeta/src/Functions/MobileCheck.js
@@ -4,7 +4,7 @@ function MobileCheck() {
   const [isMobile, setIsMobile] = useState(false);
   const bodySearch = document.querySelector('.bodysearchpage')
   const searchMessage = `<p style="color:#fff;text-align:center;"><strong>Please view this site on a desktop browser for the best user experience.</strong></p>`
-  const homeMesage =    <p> <strong>Please view this site on a desktop browser for the user best experience. </strong></p>
+  const homeMesage =    <p> <strong>Please view this site on a desktop browser for the best user experience. </strong></p>
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 768px)");
     setIsMobile(mediaQuery.matches);
