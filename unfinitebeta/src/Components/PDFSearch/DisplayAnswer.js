cf1133b64427dc1b9e0a81ce08294cca2df8e20b
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-04-15 15:52:00 -0400

    --- special_id

--- unfinitebeta/src/Components/PDFSearch/DisplayAnswer.js
+++ unfinitebeta/src/Components/PDFSearch/DisplayAnswer.js
@@ -3,10 +3,29 @@ import PremiumButton from "./premiumbutton";
 
 function DisplayAnswer({ answer }) {
 
+    // a function that takes in the answer and finds any chunk of text enclosed b/w `` and `` and highlishts it
+    const highlight = (answer) => {
+        let highlighted = answer.split('`').map((item, index) => {
+            if (index % 2 === 0) {
+                return item;
+            }
+            return <span className="highlight">{item}</span>
+        })
+        console.log(highlighted);
+        return highlighted;
+    }
+    
+    const [highlighted, setHighlighted] = useState(highlight(answer));
+
+    useEffect(() => {
+        setHighlighted(highlight(answer));
+    }, [answer])
+
     return (
         <>
             <div className='displayanswer'>
-                <p className="answer">{answer}</p>
+                {/* <p className="answer">{highlight}</p> */}
+                <p className="answer">{highlighted}</p> 
                 {/* <PremiumButton /> */}
             </div>
         </>
--- unfinitebeta/src/Functions/PDFSearch/answerQuestion.js
+++ unfinitebeta/src/Functions/PDFSearch/answerQuestion.js
@@ -3,7 +3,7 @@ import getCSRF from "../getCSRF";
 import { API_URL } from "../../API_URL";
 import getCookie from "../getCookie";
 
-async function answerQuestion(question, docids, threadid) {
+async function answerQuestion( question, docids, threadid , special_id ) {
   getCSRF();
   const csrfToken = getCookie("csrftoken");
 
@@ -19,6 +19,7 @@ async function answerQuestion(question, docids, threadid) {
       'question': question,
       'docids': JSON.stringify(docids),
       'threadid': threadid,
+      'special_id': special_id,
     }),
   }).then(response => response.json()).catch(error => console.log(error));
 
