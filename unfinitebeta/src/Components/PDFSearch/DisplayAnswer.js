d1e73ebfb1d50895d1b5dad54d28529f6f8b3c0d
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2023-04-26 19:18:52 -0400

    --- KPs

--- unfinitebeta/src/Components/PDFSearch/DisplayAnswer.js
+++ unfinitebeta/src/Components/PDFSearch/DisplayAnswer.js
@@ -15,52 +15,91 @@ import LikeDislike from "./LikeDislike";
 //   return qids;
 // }
 
+function renderTextWithKPsandQs( { answer , handleQuestionclick } ) {
+
+  let parts = [];
+  let subparts = [];
+
+  if (!answer) {
+    return null; // or return an empty array or default value
+  }
+
+  const partslayerone = answer.split(/({.*?})/);
+
+  partslayerone.forEach((part, index) => {
+
+    subparts = part.split(/(<kp>.*?<\/kp>)/);
+
+    subparts.forEach((subpart, index) => {
+
+      console.log(subpart);
+
+      parts.push(subpart);
+
+    });
+
+  });
+
+
+  return parts.map((part, index) => {
+    console.log(part);
+    if (part.startsWith("<kp>") && part.endsWith("</kp>")) {
+      return <span key={index} className="kp" onClick={handleQuestionclick}>{part.slice(4, -5)}</span>;
+    } else if (part.startsWith("{") && part.endsWith("}")) {
+      return <span key={index} className="qs" onClick={handleQuestionclick}>Q. {part.slice(1, -1)}</span>;
+    } else {
+      return part;
+    }
+  });
+}
+
 
 function DisplayAnswer({ qid , answer , nextquestion , setNextquestion , currentquestion }) {
 
     const [thumbs, setThumbs] = useState(0);
     const [specialresponse, setSpecialresponse] = useState(null);
     let displayanswer = null;
+    
 
     // find questions in the answer that are encapsulated by curly braces and return an array of the text not in curly braces and the text in curly braces
-    const findQs = (text) => {
-        const regex = /{([^}]+)}/g;
-        const matches = text.match(regex);
-        const qids = [];
-        if (matches) {
-            matches.forEach((match) => {
-                qids.push(match.slice(1, -1));
-            });
-        }
-        return qids;
-    };
+    // const findQs = (text) => {
+    //     const regex = /{([^}]+)}/g;
+    //     const matches = text.match(regex);
+    //     const qids = [];
+    //     if (matches) {
+    //         matches.forEach((match) => {
+    //             qids.push(match.slice(1, -1));
+    //         });
+    //     }
+    //     return qids;
+    // };
 
     // set displayanswer to the answer without the questions in curly braces
-    const highlight = (text) => {
-        const regex = /{([^}]+)}/g;
-        const matches = text.match(regex);
-        const qids = [];
-        if (matches) {
-            matches.forEach((match) => {
-                qids.push(match.slice(1, -1));
-            });
-        }
-        let displayanswer = text;
-        qids.forEach((qid) => {
-            displayanswer = displayanswer.replace(`{${qid}}`, "");
-        });
+    // const highlight = (text) => {
+    //     const regex = /{([^}]+)}/g;
+    //     const matches = text.match(regex);
+    //     const qids = [];
+    //     if (matches) {
+    //         matches.forEach((match) => {
+    //             qids.push(match.slice(1, -1));
+    //         });
+    //     }
+    //     let displayanswer = text;
+    //     qids.forEach((qid) => {
+    //         displayanswer = displayanswer.replace(`{${qid}}`, "");
+    //     });
 
-        // strip the displayanswer of unnecessary spaces
-        // displayanswer = displayanswer.replace(/\s+/g, " ").trim();
+    //     // strip the displayanswer of unnecessary spaces
+    //     // displayanswer = displayanswer.replace(/\s+/g, " ").trim();
 
-        // strip the unnecessary spaces from the start and end of the displayanswer
-        displayanswer = displayanswer.trim();
+    //     // strip the unnecessary spaces from the start and end of the displayanswer
+    //     displayanswer = displayanswer.trim();
 
-        // replace a space if len of the space is greater than 1 with a single space
-        // displayanswer = displayanswer.replace(/\s{2,}/g, " ");
+    //     // replace a space if len of the space is greater than 1 with a single space
+    //     // displayanswer = displayanswer.replace(/\s{2,}/g, " ");
 
-        return displayanswer;
-    };
+    //     return displayanswer;
+    // };
 
     useEffect(() => {
         console.log(thumbs);
@@ -96,17 +135,17 @@ function DisplayAnswer({ qid , answer , nextquestion , setNextquestion , current
     <>
       <div className="displayanswer">
         <AnswerComplexity setSpecialresponse={setSpecialresponse} />
-        {/* <p className="answer">{highlight}</p> */}
-        {/* <p className="answer">{answer}</p> */}
+        {/* <p className="answer"> */}
+          {/* {highlight(answer)} */}
+          <p className="answer">
+          {/* {answer} */}
+          {renderTextWithKPsandQs({ answer , handleQuestionclick })}
+          </p>
+          {/* </p> */}
         {/* {findQs(answer).map((qid) => {
-          return <p className="qid">{qid}</p>;
-        }
-        )} */}
-        <p className="answer">{highlight(answer)}</p>
-        {findQs(answer).map((qid) => {
           return <p className="answer">{"Q. "}<span className="qs" onClick={handleQuestionclick}>{qid}</span></p>;
         }
-        )}
+        )} */}
         {/* { answer ? <LikeDislike setThumbs={setThumbs} ></LikeDislike> : null} */}
       </div>
     </>
